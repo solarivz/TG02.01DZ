3078cc795e49468c50363afa80e385bbdb84376a
Ivan Zyulin <solar.ivz@gmail.com>
2024-12-17 22:05:23 +1200

    add handler start , init bot

--- main.py
+++ main.py
@@ -10,3 +10,35 @@ from googletrans import Translator
 from config import TOKEN
 
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger("aiogram")
+
+# Создание бота и диспетчера
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+# Инициализация переводчика
+translator = Translator()
+
+# ================= Хэндлеры ================= #
+
+# 1. Хэндлер для команды /start
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(f"Привет, {message.from_user.full_name}!\n"
+                         "Я сохраню твои фото, отправлю голосовые сообщения и переводу текст на английский язык.")
+
+
+
+
+# ================= Запуск бота ================= #
+async def main():
+    logging.info("Бот запущен!")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.error("Бот был остановлен!")
